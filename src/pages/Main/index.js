b08c49e1ae8e627dd1580ff694f99be714dd1f8a
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-11-13 11:48:56 -0300

    styled Card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import Icon from 'expo-vector-icons/MaterialIcons';
 
 import Header from '../../components/Header';
 import Tabs from '../../components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Annotation,
+  Description,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Bruno Santos hoje às 06:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
