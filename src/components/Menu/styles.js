4073c53982fdac939e76aaa7af2f37b33b9ff5d0
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-11-13 16:08:48 -0300

    add animation Card and Menu

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,7 +1,8 @@
-import { StyleSheet } from 'react-native';
+import { Animated, StyleSheet } from 'react-native';
+
 import styled from 'styled-components/native';
 
-export const Container = styled.ScrollView.attrs({
+export const Container = styled(Animated.ScrollView).attrs({
   showsVerticalScrollIndicator: false,
 })`
   margin: 0 30px;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Icon from 'expo-vector-icons/MaterialIcons';
 
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
 import Header from '../../components/Header';
 import Tabs from '../../components/Tabs';
 import Menu from '../../components/Menu';
@@ -18,29 +21,61 @@ import {
 } from './styles';
 
 export default function Main() {
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true }
+  );
+
+  function onHandlerStateChanged(event) {}
+
   return (
     <Container>
       <Header />
 
       <Content>
-        <Menu />
+        <Menu translateY={translateY} />
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [-350, 0, 350],
+                    outputRange: [-50, 0, 350],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
 
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 197.611,65</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 20,00 recebida de Bruno Santos hoje às 06:00h
-            </Annotation>
-          </CardFooter>
-        </Card>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 197.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de Bruno Santos hoje às
+                06:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
       <Tabs />
