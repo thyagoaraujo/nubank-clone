b2b21a5b197c1ad9aafbbd0b10d829953dfea0b3
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-11-13 10:13:51 -0300

    add background, statusBar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 
-const Index = () => <Routes />;
+const Index = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#8b10ae" />
+    <Routes />
+  </>
+);
 
 export default Index;
